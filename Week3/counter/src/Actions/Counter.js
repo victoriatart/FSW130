bf0d859943f719f81cd5614731ce2fdffc8a3593
victoriatart <68974738+victoriatart@users.noreply.github.com>
2021-05-31 20:58:14 -0400

    Add files via upload

---
+++ Week3/counter/src/Actions/Counter.js
@@ -0,0 +1,7 @@
+import ActionTypes from '../Actions'
+export const increaseCount = () => ({
+type: ActionTypes.INCREASE_COUNT
+})
+export const decreaseCount = () => ({
+type: ActionTypes.DECREASE_COUNT
+})
---
+++ Week3/counter/src/Actions/index.js
@@ -0,0 +1,7 @@
+const CounterTypes = {
+    INCREASE_COUNT: 'INCREASE_COUNT',
+    DECREASE_COUNT: 'DECREASE_COUNT'
+    }
+    export default {
+    ...CounterTypes,
+    }
